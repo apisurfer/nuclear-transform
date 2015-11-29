a166927c5e7c36df38da04ef6cee28c48eb63fad
Luka Vidakovic <luka.vidakovic@gmail.com>
2015-11-29 17:12:03 +0100

    Pass context to reducers

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var isString = fighterr.isString;
 var isFunction = fighterr.isFunction
 var isArray = fighterr.isArray;
 
-function reduce(getter, reducer) {
+function reduce(getter, reducer, ctx) {
   var reducerIsFunction = isFunction(reducer);
   var reducerIsArray = isArray(reducer);
 
@@ -18,24 +18,25 @@ function reduce(getter, reducer) {
   if (reducerIsFunction) {
     return [
       getter,
-      reducer,
+      reducer.bind(ctx),
     ];
   }
 
   // otherwise reducer is defined through array
   if (isString(getter[0])) {
-    return [[getter]].concat(reducer);
+    return [[getter]].concat(reducer.bind(ctx));
   }
 
-  return [getter].concat(reducer);
+  return [getter].concat(reducer.bind(ctx));
 }
 
 function transform(getterList) {
+  var ctx = {};
   var wrapped = getterList.shift();
   var next = getterList.shift();
 
   while (next) {
-    wrapped = reduce(wrapped, next);
+    wrapped = reduce(wrapped, next, ctx);
     next = getterList.shift();
   }
 
